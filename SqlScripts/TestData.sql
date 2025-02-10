9893265c5472c015e0902ddcfa9b6f3add075858
Naveen <86683623+Naveenselvadurai@users.noreply.github.com>
2025-02-10 21:30:43 +0530

    added test data
    added test data

---
+++ SqlScripts/TestData.sql
@@ -0,0 +1,13 @@
+INSERT INTO Books (Publisher, Title, AuthorFirstName, AuthorLastName, Price)
+VALUES
+('Penguin India', 'The White Tiger', 'Aravind', 'Adiga', 399.50),
+('HarperCollins India', 'Midnight’s Children', 'Salman', 'Rushdie', 499.00),
+('Rupa Publications', 'The Immortals of Meluha', 'Amish', 'Tripathi', 350.00),
+('Aleph Book Company', 'A Suitable Boy', 'Vikram', 'Seth', 799.00),
+('Penguin India', 'The God of Small Things', 'Arundhati', 'Roy', 450.00),
+('Westland Books', 'The Palace of Illusions', 'Chitra', 'Banerjee', 399.00),
+('Rupa Publications', 'Five Point Someone', 'Chetan', 'Bhagat', 250.00),
+('Jaico Publishing House', 'Autobiography of a Yogi', 'Paramahansa', 'Yogananda', 299.00),
+('Penguin India', 'Train to Pakistan', 'Khushwant', 'Singh', 375.00),
+('Aleph Book Company', 'The Inheritance of Loss', 'Kiran', 'Desai', 420.00);
+
